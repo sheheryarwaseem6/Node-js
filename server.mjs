1441e988e40b126f18aebb1266457b9d6a263b69
saad-chohan <86551822+saad-chohan@users.noreply.github.com>
2021-11-01 21:26:51 +0500

    Node js

---
+++ server.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+ const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("I am a hello world server");
+})
+app.get("/profile", (req, res) => {
+    res.send("here is your profile");
+})
+app.get("/about", (req, res) => {
+    res.send("some information about me");
+})
+app.listen(3000, () => {
+    console.log(`server is running on port 3000`);
+});
